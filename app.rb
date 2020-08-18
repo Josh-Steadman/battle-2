2bc6f39213227be6c259fdee9061440b9d231428
joshSteadman <josh.steadman15@gmail.com>
2020-08-18 12:59:40 +0100

    new folder

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+
+configure(:development) { set :session_secret, "keeps cute cat alive" }
+
+get '/' do
+  "Hello!!"
+end
+
+get '/secret' do
+  'its a secret'
+end
+
+get '/battle_plan' do
+  'archers at the back'
+end
+
+get '/location' do
+  'Asda car park!'
+end
+
+get '/random-cat' do
+    @names= ["Amigo", "Oscar", "Viking"].sample
+    erb(:index)
+end
+
+get '/named-cat' do
+    p params
+    @name = params[:name]
+    erb(:index)
+end
